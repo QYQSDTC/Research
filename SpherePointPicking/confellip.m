3ea7f7323afa94218968e1fd5c5733702e308441
QYQ <qyqstc@gmail.com>
2017-01-20 00:07:52 +0800

    update the confellip.m and creat a test file for it

--- SpherePointPicking/confellip.m
+++ SpherePointPicking/confellip.m
@@ -3,19 +3,48 @@
 % Sep 26, 2016. YW
 % Jan 18, 2017. Yi-Qian Qian added contour and Monte Carlo simulation 
 
-clear;
-
+function [SD1,SD2]=confellip(XX,YY,varargin)
+if nargin < 3
+   % fprintf('Use the default confidence levels 68%% and 95%%');
+    disp('Use the default confidence levels 68% and 95%');
+end
+if nargin == 3 || nargin == 5
+    error('The input argument is wrong');
+end
+conf1 = 0.68; %default confidence level 1
+conf2 = 0.95; %default confidence level 2
+pp = varargin;
+while length(pp) >= 2
+    prop = pp{1};
+    if isa(pp{2},'double')
+        val = pp{2};
+    else
+        error('The input argument is wrong');
+    end
+    pp = pp(3:end);
+    switch prop
+        case 'conf1'
+            conf1 = val;
+        case 'conf2'
+            conf2 = val;
+        otherwise
+            error('The input argument is wrong');
+    end
+end
+    
 % load data into workspace
 %results=load('/Users/ywang/Research/PULSARTIMING/MultiCW/simDataSKA_loc6/resultsSKA_snr0123_AnglesWrapped_v1_May26_2016/summary/H1stats_snr1_loc6_omg3.mat');
 %results=load('F:\GitProject\MatlabPrograms\SpherePointPicking/H1stats_snr1_loc9_omg3.mat');
-results=load('/Users/ywang/Research/PULSARTIMING/MultiCW/simDataSKA_loc9/resultsSKA_snr0123_loc9/summary/H1stats_snr1_loc9_omg3.mat');
+%results=load('/Users/ywang/Research/PULSARTIMING/MultiCW/simDataSKA_loc9/resultsSKA_snr0123_loc9/summary/H1stats_snr1_loc9_omg3.mat');
 
-data=[results.estSigParams(:,1), results.estSigParams(:,2)]; % x, y coords 
-Ns=length(results.estSigParams(:,1));  % sample size
-N68 = floor(Ns*0.68);  % contour level
-N95 = floor(Ns*0.95);  % contour level
-ra=mean(data(:,1));  % mean value of RA
-dec=mean(data(:,2));  % mean value of Dec
+%data=[results.estSigParams(:,1), results.estSigParams(:,2)]; % x, y coords 
+data(:,1)=XX;
+data(:,2)=YY;
+Ns=length(XX);  % sample size
+Conf1 = floor(Ns*conf1);  % contour level
+Conf2 = floor(Ns*conf2);  % contour level
+ra=mean(XX);  % mean value of RA
+dec=mean(YY);  % mean value of Dec
 
 % calculate the density by 'kde2d' function
 [bandwidth,density,X,Y]=kde2d(data);
@@ -27,8 +56,8 @@ figure;
 level=zeros(Ncl,1);  % contour level
 
 Np=zeros(Ncl,1);  % number of date points located inside that contour
-Np68=zeros(3,1);  % number of random sphere points located inside that contour
-Np95=zeros(3,1);  % number of random sphere points located inside that contour
+NpConf1=zeros(3,1);  % number of random sphere points located inside that contour
+NpConf2=zeros(3,1);  % number of random sphere points located inside that contour
 cont=1;  % counter
 xq=data(:,1);  % x coord.
 yq=data(:,2);  % y coord.
@@ -56,10 +85,10 @@ for i=1:1:Ncl
     
 end
 
-% generate the N68 contour
+% generate the Conf1 contour
 for i=1:1:Ncl
-    if Np(i)<N68
-        disp(['Index for 68% level is: ', num2str(i), ', Np= ', num2str(Np(i))...
+    if Np(i)<Conf1
+        disp(['Index for ',num2str(conf1*100),'% level is: ', num2str(i), ', Np= ', num2str(Np(i))...
             ', contour level is: ', num2str(level(i))]);
         break;
     end
@@ -77,29 +106,30 @@ for i=1:1:i1
     
     %xv=zeros(Nc,1);  % x coord. of contour
     %yv=zeros(Nc,1);
-    xv=C(1,int:fin)';
-    yv=C(2,int:fin)';
+    xv1=C(1,int:fin)';
+    yv1=C(2,int:fin)';
     
-    in=inpolygon(theta,phi,xv,yv);
-    Np68(i)=sum(in);
-    P = Np68(i)/n ; %calculate the fraction of N68 to the solid angle
+    in=inpolygon(theta,phi,xv1,yv1);
+    NpConf1(i)=sum(in);
+    P = NpConf1(i)/n ; %calculate the fraction of N68 to the solid angle
 end
-disp(['The fraction of N68 to the solid angle is :',num2str(P)]);
-disp(['Square degree for N68 is :',num2str(P*41253.0)]);
-figure;
-plot(data(:,1),data(:,2),'g+');
-hold on;
-plot(theta,phi,'.r');
-hold on;
-plot(xv,yv,'k');
-xlim([0 2*pi])
-ylim([-pi/2 pi/2])
-title('68% confidence contour');
+SD1 = P*41253.0;
+disp(['The fraction of ',num2str(conf1*100),'% to the solid angle is :',num2str(P)]);
+disp(['Square degree for ',num2str(conf1*100),'% is :',num2str(SD1)]);
+% figure;
+% plot(data(:,1),data(:,2),'g+');
+% hold on;
+% % plot(theta,phi,'.r');
+% % hold on;
+% plot(xv,yv,'k');
+% xlim([0 2*pi])
+% ylim([-pi/2 pi/2])
+% title('68% confidence contour');
 
 % generate the N95 contour
 for i=1:1:Ncl
-    if Np(i)<N95
-        disp(['Index for 95% level is: ', num2str(i), ', Np= ', num2str(Np(i))...
+    if Np(i)<Conf2
+        disp(['Index for ',num2str(conf2*100),'% level is: ', num2str(i), ', Np= ', num2str(Np(i))...
             ', contour level is: ', num2str(level(i))]);
         break;
     end
@@ -117,25 +147,29 @@ for i=1:1:i2
     
     %xv=zeros(Nc,1);  % x coord. of contour
     %yv=zeros(Nc,1);
-    xv=C(1,int:fin)';
-    yv=C(2,int:fin)';
+    xv2=C(1,int:fin)';
+    yv2=C(2,int:fin)';
     
-    in=inpolygon(theta,phi,xv,yv);
-    Np95(i)=sum(in);
-    P = Np95(i)/n ; % calculate the fraction of N95 to the solid angle
+    in=inpolygon(theta,phi,xv2,yv2);
+    NpConf2(i)=sum(in);
+    P = NpConf2(i)/n ; % calculate the fraction of N95 to the solid angle
     
 end
-disp(['The fraction of N95 to the solid angle is :',num2str(P)]);
-disp(['Square degree for N95 is :',num2str(P*41253.0)]);
+SD2 = P*41253.0;
+disp(['The fraction of ',num2str(conf2*100),'% to the solid angle is :',num2str(P)]);
+disp(['Square degree for ',num2str(conf2*100),'% is :',num2str(SD2)]);
 figure;
-plot(data(:,1),data(:,2),'g+');
+plot(data(:,1),data(:,2),'m+');
 hold on;
-plot(theta,phi,'.r');
-hold on;
-plot(xv,yv,'k');
+% plot(theta,phi,'.r');
+% hold on;
+plot(xv1,yv1,'k');
+hold on
+plot(xv2,yv2,'b')
+legend('data points',num2str(conf1),num2str(conf2));
 xlim([0 2*pi])
 ylim([-pi/2 pi/2])
-title('95% confidence contour');
+%title('95% confidence contour');
 %contour3(X,Y,density,[121.9 121.9])
 
 % End
---
+++ SpherePointPicking/testConfellip.m
@@ -0,0 +1,11 @@
+%The test file for confellip.m
+clear;
+results=load('F:\GitProject\MatlabPrograms\SpherePointPicking/H1stats_snr1_loc9_omg3.mat');
+XX=results.estSigParams(:,1);
+YY=results.estSigParams(:,2);
+[SD1,SD2]=confellip(XX,YY,'conf1',0.8,'conf2',0.90);
+% %syntax: SD1 is the square degree for conf1
+%          SD2 is the square degree for conf2
+%          conf1 is confidence level 1
+%          conf2 is confidence level 2 
+%          if conf1 and conf2 are missing, using the default confidence levels for conf1=0.68, conf2=0.95
