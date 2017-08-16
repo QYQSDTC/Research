cb22179f7cb1c27d119852bf8e5d467f9d8606d3
Dr.QYQ <qyqstc@gmail.com>
2017-08-16 11:06:56 +0800

    update

--- KIAA Intern/simulation.m
+++ KIAA Intern/simulation.m
@@ -45,19 +45,20 @@ yy=reshape(signal,[1 BW*N]);
 %     hold on
 % end
 % 
-% for i = 2:1:BW
-% tao(i)=4150*DM*(ch(i)^(-2)-ch(i-1)^(-2));
-% gt=snr*normpdf(np,sum(tao(1:i,:)),0.1);
-% g(:,i)=gt;
-% signal(:,i)=signal(:,i-1)+gt';
-% end
-% 
-% figure 
-% plot(np,signal(:,BW))
-% 
-% fit_np=np';% to import to origin
-%save('fit_x.txt','fit_np','-ascii')
-%save('fit_y.txt','signal','-ascii')
+for i = 2:1:BW
+tao(i)=4150*DM*(ch(i)^(-2)-ch(i-1)^(-2));
+gt=snr*normpdf(np,sum(tao(1:i,:)),0.1);
+g(:,i)=gt;
+signal(:,i)=signal(:,i-1)+gt';
+end
+
+figure 
+plot(np,signal(:,BW))
+
+fit_np=np';% to import to origin
+save('fit_x.txt','fit_np','-ascii')
+save('fit_y.txt','signal','-ascii')
+save('weights.txt','weights','-ascii')
 
 %% curve fitting
 % ft=fittype('gauss1');
@@ -80,9 +81,10 @@ yy=reshape(signal,[1 BW*N]);
 % y=signal(3,:);
 % [p3,gof3]=fit(t(:),y(:),ft3);
 mu=zeros(BW,1);
-for i=3:1:3
+for i=1:1:BW
     options=fitoptions('Weights',weights);
-    ft=fittype('gauss3');
+    %ft=fittype('gaussfit(x,a1,b1,c1,a2,b2,c2,a3,b3,c3,a4,b4,c4,a5,b5,c5,a6,b6,c6,a7,b7,c7,a8,b8,c8,a9,b9,c9,a10,b10,c10,a11,b11,c11,a12,b12,c12,a13,b13,c13,a14,b14,c14,a15,b15,c15,a16,b16,c16,a17,b17,c17,a18,b18,c18,a19,b19,c19,a20,b20,c20,a21,b21,c21,a22,b22,c22,a23,b23,c23,a24,b24,c24,a25,b25,c25,a26,b26,c26,a27,b27,c27,a28,b28,c28,a29,b29,c29,a30,b30,c30,a31,b31,c31)');
+    ft=fittype('gauss8');
     y=signal(:,i);
     [p,~]=fit(np(:),y(:),ft,options);
     mu(i)=p.b1;
