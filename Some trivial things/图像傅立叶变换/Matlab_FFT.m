25d94c0dfb31585c5c1ffd562654270c938bbbe2
Dr.QYQ <qyqstc@gmail.com>
2018-07-06 11:45:05 +0800

    add matlab fft test

---
+++ Some trivial things/图像傅立叶变换/Matlab_FFT.m
@@ -0,0 +1,13 @@
+clc;
+clear;
+A=imread('_WSH0024.jpg');
+if ndims(A)==3
+   A=rgb2gray(A);% 3D to 2D
+end
+I=fft2(double(A));% change data to type double
+B=fftshift(I);% move the zero zone to the center
+C=log(1+abs(B));% show beatifully
+D = real(ifft2(I));
+subplot(1,3,1),imshow(A),title('oringinal image');
+subplot(1,3,2),imshow(C,[]),title('image of FFT');
+subplot(1,3,3),imshow(D,[]),title('image of inverse FFT');
