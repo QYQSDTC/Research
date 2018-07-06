383384b85a86e50a2606723bc600fa711884a268
Dr.QYQ <qyqstc@gmail.com>
2018-07-06 13:18:35 +0800

    update

--- Some trivial things/Matlab FFT Test/Matlab_FFT.m
+++ Some trivial things/Matlab FFT Test/Matlab_FFT.m
@@ -8,8 +8,8 @@ I=fft2(double(A));% change data to type double
 B=fftshift(I);% move the zero zone to the center
 C=log(1+abs(B));% show beatifully
 D = real(ifft2(I));
-% subplot(1,2,1),imshow(A),title('oringinal image');
-% subplot(1,2,2),imshow(C,[]),title('image of FFT');
-%subplot(1,3,3),imshow(D,[]),title('image of inverse FFT');
- figure
- imshow(C,[])
+subplot(1,3,1),imshow(A),title('oringinal image');
+subplot(1,3,2),imshow(C,[]),title('image of FFT');
+subplot(1,3,3),imshow(D,[]),title('image of inverse FFT');
+%  figure
+%  imshow(C,[])
